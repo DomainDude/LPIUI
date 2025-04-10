a80e2becd0575e2d714c8af397e6d56e61a01ccd
DomainDude <vgxlmangrego@gmail.com>
2025-04-11 01:16:11 +0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -121,7 +121,7 @@ Voicehahsbs = "",
 kingmatviyy = "",
 ["123lush123b"] = "",
 Pickle345790 = "",
-PlayStationPlayer = "", --Lil bro forgot to close hard gay porn so it was on his mic. bitch ass vampire same as jojos. not skid but cocksucker.
+PlayStationPlayer = "", --Lil bro forgot to close hard gay porn so it was on his mic. bitch ass vampire "Dio" same as jojos. him is vampire because him need to suck sperm to live.
 jojos_351 = "", --[[ Bitch Dont Look Inside Here, Before I Put another Dio arrow In You Fucking Skid. Whole Fucking army Gonna Be against You Bitch, I aint Playing With Your Titties. Hoe. Piece Of Frozen Cum. 
   Suck My Cock Dont Forget To Suck Other Members Bitch. Fucking Vampire I Will Remove Shadows Bitch.]]
 }
