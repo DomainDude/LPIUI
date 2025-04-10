24c0e64c416b2003f72a90917646814b04ac40fe
DomainDude <vgxlmangrego@gmail.com>
2025-04-11 01:03:15 +0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,1 +1,13 @@
-return 
+return {
+BullsOfStell = "",
+BullsOfStell1 = "",
+JunkDunkIn = "",
+Manulexy_3010 = "",
+XoxTimoxaxoX = "",
+nothin_057 = "",
+Noob_CheapingBlock = "",
+Nolar_Asker = "",
+olithebrave1 = "",
+Manulexy_3009 = "",
+Drd3_jailbreak = "",
+}
