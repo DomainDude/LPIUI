a76f887d39983ef93a5e840be6c26ed497946c82
DomainDude <vgxlmangrego@gmail.com>
2025-04-11 02:27:56 +0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -123,5 +123,9 @@ kingmatviyy = "",
 Pickle345790 = "",
 PlayStationPlayer = "", --Lil bro forgot to close hard gay porn so it was on his mic. bitch ass vampire "Dio" same as jojos. him is vampire because him need to suck sperm to live.
 jojos_351 = "", --[[ Bitch Dont Look Inside Here, Before I Put another Dio arrow In You Fucking Skid. Whole Fucking army Gonna Be against You Bitch, I aint Playing With Your Titties. Hoe. Piece Of Frozen Cum. 
-  Suck My Cock Dont Forget To Suck Other Members Bitch. Fucking Vampire I Will Remove Shadows Bitch.]]
+  Suck My Cock Dont Forget To Suck Other Members Bitch. Fucking Vampire I Will Remove Shadows Bitch.
+  I Didnt Ended It jojos_351, The Numbers In UserName Is How Much You Got Recored In Hard Gay Porn, Fucking BI. Transgender, Bitch, Best Sperm Vacum For Free.
+  lil bro is one of the someone who said "Youre The King" In This Gay ahh Bypassed Sound.
+  Bro Is Fucking Skid. acid Fuck You, I aint playing with you.
+  #SKID #SKID SUCK MY DICK.]]
 }
